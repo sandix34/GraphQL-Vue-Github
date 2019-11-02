202376d8d27a0b7cff0093875aabcb56663232f7
sandrine <sandrinezocca@gmail.com>
2019-11-02 17:29:19 +0100

    mise en forme avec tailwind css

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="hello">
-    <h1 class="text-5xl font-semibold">Mes référentiels</h1>
-    <div v-for="repo in repositoryOwner.repositories.edges" :key="repo.id">
-      <p>{{ repo.node.createdAt }}</p>
-      <h3>{{ repo.node.name }}</h3>
-      <p v-html="repo.node.descriptionHTML"></p>
-      <a :href="repo.node.url">Voir le référentiel</a>
+  <div class="w-11/12 m-auto">
+    <h1 class="text-5xl font-semibold p-5">Mes référentiels</h1>
+    <div class="flex flex-wrap">
+      <div v-for="repo in repositoryOwner.repositories.edges" :key="repo.id" class="bg-gray-100 customshadow rounded-lg w-64 m-8 p-4">
+      <p class="text-left pb-4">{{ repo.node.createdAt }}</p>
+      <h3 class="text-left text-xl font-bold pb-2">{{ repo.node.name }}</h3>
+      <p v-html="repo.node.descriptionHTML" class="text-left leading-snug text-xl text-gray-700"></p>
+      <a :href="repo.node.url" class="text-center bg-gray-200 rounded-full block mt-4 text-gray-900 hover:underline w-40 p-1">Voir le référentiel</a>
+    </div>
     </div>
   </div>
 </template>
@@ -27,5 +29,12 @@ export default {
 </script>
 
 <style scoped>
-
+.customshadow {
+  box-shadow: 0 1px 1px rgba(0,0,0,0.11), 
+              0 2px 2px rgba(0,0,0,0.11), 
+              0 4px 4px rgba(0,0,0,0.11), 
+              0 8px 8px rgba(0,0,0,0.11), 
+              0 16px 16px rgba(0,0,0,0.11), 
+              0 32px 32px rgba(0,0,0,0.11);
+}
 </style>
