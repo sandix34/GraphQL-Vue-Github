f96af4b928e15bdcf531947dbb4b2a5289c8a1d1
sandrine <sandrinezocca@gmail.com>
2019-11-03 13:29:44 +0100

    gérer le format de la date

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,7 +3,7 @@
     <h1 class="text-5xl font-semibold p-5">Mes référentiels</h1>
     <div class="flex flex-wrap">
       <div v-for="repo in repositoryOwner.repositories.edges" :key="repo.id" class="bg-gray-100 customshadow rounded-lg w-64 m-8 p-4">
-      <p class="text-left pb-4">{{ repo.node.createdAt }}</p>
+      <p class="text-left pb-4">{{ formatDate(repo.node.createdAt) }}</p>
       <h3 class="text-left text-xl font-bold pb-2">{{ repo.node.name }}</h3>
       <p v-html="repo.node.descriptionHTML" class="text-left leading-snug text-xl text-gray-700"></p>
       <a :href="repo.node.url" class="text-center bg-gray-200 rounded-full block mt-4 text-gray-900 hover:underline w-40 p-1">Voir le référentiel</a>
@@ -23,6 +23,21 @@ export default {
   data() {
     return {
       repositoryOwner: []
+    }
+  },
+  methods: {
+    // convertir une date ISO 
+    formatDate(date) {
+    
+      let isoDate = date;
+      let d = new Date(isoDate);
+      const options = {
+         year: 'numeric',
+         month: 'long',
+         day: 'numeric'
+      }
+      
+      return d.toLocaleDateString('fr-FR', options);
     }
   },
 };
